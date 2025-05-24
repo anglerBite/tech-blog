92d5ffd8627f5d6851cc4f860eeea4fde82a7569
anglerBite <jackall489@gmail.com>
2025-05-24 21:30:20 +0900

    修正

--- src/app/_component/Article/Article.tsx
+++ src/app/_component/Article/Article.tsx
@@ -5,20 +5,16 @@ import Link from 'next/link';
 import styles from './Article.module.css';
 
 const Article = async () => {
-    const data = await getData();
+
+    const data: MockData[] = await getData();
 
     return (
         <div className={styles.container}>
-            {data.map((item: MockData) => (
+            {data.map((item) => (
                 <article key={item.id} className={styles.article}>
                     <Link href={`/text/${item.id}`} className={styles.articleLink}>
                         <div className={styles.articleMeta}>
-                            <span className={styles.category}>
-                                {/* 型安全なアクセス */}
-                                {typeof item.category[0] === 'object' && item.category[0].name ? 
-                                    item.category[0].name : 
-                                    String(item.category[0])}
-                            </span>
+                            <span className={styles.category}>{item.category[0].name}</span>
                             <span className={styles.dot}>.</span>
                             <span className={styles.date}>{dayjs(item.date).format('YYYY.MM.DD')}</span>
                         </div>
@@ -27,7 +23,7 @@ const Article = async () => {
                 </article>
             ))}
         </div>
-    );
-};
+    )
+}
 
-export default Article;
+export default Article
