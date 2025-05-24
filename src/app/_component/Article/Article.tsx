d50dd675a2796d8d617a8416800e47c245436667
anglerBite <jackall489@gmail.com>
2025-05-25 03:11:54 +0900

    修正

--- src/app/_component/Article/Article.tsx
+++ src/app/_component/Article/Article.tsx
@@ -1,34 +1,31 @@
-// src/app/_component/Article/Article.tsx
 import { getData } from '@/app/lib/fetchGET';
 import dayjs from 'dayjs';
 import { MockData } from '@/types/types';
 import Link from 'next/link';
 import styles from './Article.module.css';
 
-export default async function Article() {
+const Article = async () => {
+
     const data: MockData[] = await getData();
 
     return (
         <div className={styles.container}>
-            {data.map(item => {
-                // 安全性のため optional chaining も併用
-                const firstCategoryName = item.category?.[0]?.name ?? 'Uncategorized';
-
-                return (
-                    <article key={item.id} className={styles.article}>
-                        <Link href={`/text/${item.id}`} className={styles.articleLink}>
-                            <div className={styles.articleMeta}>
-                                <span className={styles.category}>{firstCategoryName}</span>
-                                <span className={styles.dot}>.</span>
-                                <span className={styles.date}>
-                                    {dayjs(item.date).format('YYYY.MM.DD')}
-                                </span>
-                            </div>
-                            <span className={styles.articleTitle}>{item.title}</span>
-                        </Link>
-                    </article>
-                );
-            })}
+            {data.map((item) => (
+                <article key={item.id} className={styles.article}>
+                    <Link href={`/text/${item.id}`} className={styles.articleLink}>
+                        <div className={styles.articleMeta}>
+                            <span className={styles.category}>
+                                {(item.category as { name: string }[])[0].name}
+                            </span>
+                            <span className={styles.dot}>.</span>
+                            <span className={styles.date}>{dayjs(item.date).format('YYYY.MM.DD')}</span>
+                        </div>
+                        <span className={styles.articleTitle}>{item.title}</span>
+                    </Link>
+                </article>
+            ))}
         </div>
-    );
+    )
 }
+
+export default Article
